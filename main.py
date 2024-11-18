64dab6a09e7204d9edeffee80145cc02a6d92b19
Alena Klekovkina <50324020+AlyonaKlekovkina@users.noreply.github.com>
2024-11-18 15:16:51 +0100

    Update main.py
    Stage 2/4

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-# write your code here
-info = input().split()
-a = int(info[0])
-oper = info[1]
-b = int(info[2])
-result = 0
-if oper == '+':
-    result = a + b
-if oper == '-':
-    result = a - b
-if oper == '*':
-    result = a * b
-print(result)
+import random
 
+
+def generate_task():
+    a = random.randrange(2, 9+1)
+    b = random.randrange(2, 9+1)
+    oper = random.choice('+-*')
+    result = eval(f'{a}{oper}{b}')
+    return a, b, oper, result
+
+
+def simple_calculation():
+    info = input().split()
+    a = int(info[0])
+    oper = info[1]
+    b = int(info[2])
+    result = 0
+    if oper == '+':
+        result = a + b
+    if oper == '-':
+        result = a - b
+    if oper == '*':
+        result = a * b
+    return result
+
+
+task = generate_task()
+print(task[0], task[2], task[1])
+reply = input()
+if int(task[3]) == int(reply):
+    print('Right!')
+else:
+    print('Wrong!')
